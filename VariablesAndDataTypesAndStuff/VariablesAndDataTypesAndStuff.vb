03355816aab9d77007c60ebf21bf0b145fe0d31f
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-22 08:28:21 -0700

    Set integer jellyBean

--- VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
+++ VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
@@ -1,6 +1,20 @@
-﻿Module VariablesAndDataTypesAndStuff
+﻿'Payden Hoskins
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/PaydenHoskins/VariablesAndDataTypesAndStuff.git
+
+Module VariablesAndDataTypesAndStuff
+
 
     Sub Main()
+
+        Dim jellyBeans As Integer
+
+
+
+
+
 
     End Sub
 
