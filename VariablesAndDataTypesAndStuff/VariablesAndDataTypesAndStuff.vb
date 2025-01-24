6c4975d319493ab1494d0e7805be67387f5647b9
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-24 09:03:03 -0700

    calculator

--- VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
+++ VariablesAndDataTypesAndStuff/VariablesAndDataTypesAndStuff.vb
@@ -4,7 +4,13 @@
 'OperatorsExample
 'https://github.com/PaydenHoskins/VariablesAndDataTypesAndStuff.git
 
+Option Strict On
 Option Explicit On
+
+
+Imports System.Globalization
+Imports Microsoft.VisualBasic.ApplicationServices
+
 
 Module VariablesAndDataTypesAndStuff
 
@@ -13,6 +19,9 @@ Module VariablesAndDataTypesAndStuff
 
         Dim LaffyTaffy As Integer
         Dim jellyBeans As Integer
+        Dim userinput As String
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
 
         Console.WriteLine(jellyBeans)
 
@@ -27,8 +36,31 @@ Module VariablesAndDataTypesAndStuff
 
         LaffyTaffy = 26
         Console.WriteLine(LaffyTaffy)
-        'conversion
-        'datatypes
+
+        Console.WriteLine("Choose your first number")
+        userinput = Console.ReadLine()
+        firstNumber = CInt(userinput)
+        Console.WriteLine("Choose your second number")
+        userinput = Console.ReadLine()
+        secondNumber = CInt(userinput)
+
+        Console.WriteLine("Please make a selection:" & vbNewLine _
+                          & "1. subtract" & vbNewLine _
+                          & "2. add")
+        userinput = Console.ReadLine()
+
+        Console.WriteLine($"you have selecter {userinput}")
+        If userinput = "1" Then
+            Console.WriteLine("Let's subtract!")
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}")
+        ElseIf userinput = "2" Then
+            Console.WriteLine("Let's Add!")
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
+        Else
+            Console.WriteLine("INVALID, Try again later!")
+        End If
+
+
     End Sub
 
 End Module
